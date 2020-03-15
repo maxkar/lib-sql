6388b561484bd0eee9ad58d697e518280f00df21
maxkar <none@none>
2020-03-15 15:43:43 +0000

    Provide query methods.
     * Provide "update" methods
     * Provide "select" methods
     * Provide multiplicity combinators

--- src/Query.scala
+++ src/Query.scala
@@ -2,6 +2,7 @@ package ru.maxkar.lib.sql
 
 import java.sql.Connection
 import java.sql.PreparedStatement
+import java.sql.ResultSet
 
 
 /**
@@ -46,6 +47,69 @@ final class Query(fragments: Seq[String], subqueries: Seq[QueryFragment]) extend
     appendQueryText(sb)
     sb.toString
   }
+
+
+  /**
+   * Runs a database update on the given connection.
+   * @param conn connection to a database to update.
+   * @return number of rows updated.
+   */
+  def update(conn: Connection): Int =
+    withPreparedStatement(conn) { ps =>
+      ps.executeUpdate()
+    }
+
+
+  /**
+   * Runs a database update on a managed connection.
+   * @param connMgr manager of database connections.
+   * @return number of rows updated.
+   */
+  def update()(implicit connMgr: ConnectionManager): Int =
+    connMgr.withConnection(conn => update(conn))
+
+
+
+  /**
+   * Extracts (reads, selects) data using the query.
+   *
+   * The code prepares a statement, initializes all arguments, executes
+   * query and invokes the callback function.
+   *
+   * @tparam T type of the data being extracted.
+   * @param conn connection to a database to execute the query on.
+   * @param conn connMgr manager of database connections.
+   * @param callback data extraction callback.
+   * @return data extracted from the connection.
+   */
+  def select[T](conn: Connection, callback: ResultSet => T): T =
+    withPreparedStatement(conn) { ps =>
+      val rs = ps.executeQuery()
+      try {
+        callback(rs)
+      } finally {
+        rs.close()
+      }
+    }
+
+
+
+  /**
+   * Extracts (reads, selects) data using this query.
+   *
+   * The code prepares a statement, initializes all arguments, executes
+   * query and invokes the callback function.
+   *
+   * @tparam T type of the data being extracted.
+   * @param callback data extraction callback.
+   * @param connMgr manager of database connections.
+   * @return data extracted from the connection.
+   */
+  def select[T](
+        callback: ResultSet => T)(
+        implicit connMgr: ConnectionManager)
+      : T =
+    connMgr.withConnection(conn => select(conn, callback))
 
 
   override def setParameters(ps: PreparedStatement, startIdx: Int): Int = {
--- src/syntax.scala
+++ src/syntax.scala
@@ -1,5 +1,11 @@
 package ru.maxkar.lib.sql
 
+import java.sql.ResultSet
+import java.sql.SQLException
+
+import scala.collection.mutable.ArrayBuffer
+
+
 /**
  * Syntax extensions provided by the library.
  */
@@ -11,4 +17,79 @@ package object syntax {
     def sql(params: QueryFragment*): Query =
       new Query(ctx.parts, params)
   }
+
+
+  /**
+   * Creates a multi-row parser based on a single-row one.
+   * @param rowParser parser for a single row.
+   * @return parser for the whole result set.
+   */
+  def many[T](rowParser: ResultSet => T): ResultSet => Seq[T] =
+    rs => {
+      val res = new ArrayBuffer[T]
+      while (rs.next())
+        res += rowParser(rs)
+      res.toSeq
+    }
+
+
+  /**
+   * Creates a multi-row parser based on a single-row one.
+   * The resulting parser will throw an SQLException if the result
+   * set is empty.
+   * @param rowParser parser for a single row.
+   * @return parser for the whole result set.
+   */
+  def atLeastOne[T](rowParser: ResultSet => T): ResultSet => Seq[T] =
+    rs => {
+      if (!rs.next())
+        throw new SQLException("Expected at least 1 row but got none")
+
+      val res = new ArrayBuffer[T]
+      res += rowParser(rs)
+
+      while (rs.next())
+        res += rowParser(rs)
+      res.toSeq
+    }
+
+
+  /**
+   * Fetches exactly one element from result set (and ensures there are
+   * no extra elements).
+   *
+   * @param rowParser parser for a single row.
+   * @return parser for entire result set ensuring there is exactly one row.
+   */
+  def one[T](rowParser: ResultSet => T): ResultSet => T =
+    rs => {
+      if (!rs.next())
+        throw new SQLException("Expected 1 row but got none")
+
+      val res = rowParser(rs)
+
+      if (rs.next())
+        throw new SQLException("Expected 1 row but got at least 2")
+      res
+    }
+
+
+  /**
+   * Fetches at most one element from result set (and ensures there are
+   * no extra elements).
+   *
+   * @param rowParser parser for a single row.
+   * @return parser for entire result set ensuring there is at most one row.
+   */
+  def optional[T](rowParser: ResultSet => T): ResultSet => Option[T] =
+    rs => {
+      if (!rs.next())
+        None
+      else {
+        val res = rowParser(rs)
+        if (rs.next())
+          throw new SQLException("Expected at most 1 row but got at least 2")
+        Some(res)
+      }
+    }
 }
